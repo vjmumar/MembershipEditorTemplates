128d40234721535bfb7baa979caf40da06c80bb1
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-15 09:39:52 +0800

    changes

--- template1.js
+++ template1.js
@@ -2070,7 +2070,7 @@ class CourseTemplate {
          // Finally we will initialize the loading
          setTimeout(() => {
                this.initializers.initLoading(false);
-         },1000)
+         },2000)
       },
 
       initLoading: (shouldShow = true) => {

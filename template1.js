947349842356d4c722eb75b7af39d932e835aff0
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-20 17:51:52 +0800

    c

--- template1.js
+++ template1.js
@@ -3319,6 +3319,7 @@ class CourseTemplate {
          completedPosts = [],
          completedCategories = [],
       ) => {
+         console.log(completedCategories);
          // First we will create the necessary variables
          let fallbackImage =
             "https://res.cloudinary.com/dpr6hw8uh/image/upload/v1771635525/image7_w940ot.png";

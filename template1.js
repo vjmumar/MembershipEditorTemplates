265306378c1466e830c9e4af42a5c24df8ff0ab0
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-23 06:36:25 +0800

    c

--- template1.js
+++ template1.js
@@ -3338,6 +3338,7 @@ class CourseTemplate {
          categories.forEach((category) => {
             category.posts.forEach((post) => {
                const isCompleted = completedPosts.some((e) => e.id === post.id);
+               console.log(completedPosts,post)
                if (isCompleted) {
                   console.log(post, isCompleted);
                }

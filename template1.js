c5a0284d1ce9330d485bb9bf36ddb8a896019857
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-23 14:57:02 +0800

    c

--- template1.js
+++ template1.js
@@ -3135,6 +3135,7 @@ class CourseTemplate {
                this.data.fetchCategory(),
                this.data.fetchPost(),
             ]).then((res) => res.map((e) => e.value));
+         console.log(category);
 
          // Then we will retrieve and sort the posts inside the category
          const allPosts = category.category.posts.sort((a, b) =>

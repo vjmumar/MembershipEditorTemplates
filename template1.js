734c29d566dbbbcfa2b5dc5ac235ff735523396c
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-23 17:16:26 +0800

    c

--- template1.js
+++ template1.js
@@ -2830,6 +2830,7 @@ class CourseTemplate {
                   allPosts.length !== 1 && currentPostIndex !== lastPostIndexOfCategory;
                if (canGoToNextPost) {
                   const nextPost = allPosts[currentPostIndex + 1];
+                  console.log(canGoToNextPost);
                   return `<a class="template-post-page-header__arrow next" href="${`/courses/products/${nextPost?.productId}/categories/${nextPost?.categoryId}/posts/${nextPost?.id}`}"><i class="fas fa-arrow-circle-right"></i></a>`;
                }
                return "";

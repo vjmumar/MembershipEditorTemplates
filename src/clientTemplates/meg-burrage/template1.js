06d24535c766176ead38a95bd0735663ed2988ac
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-31 13:06:09 +0800

    c

--- src/clientTemplates/meg-burrage/template1.js
+++ src/clientTemplates/meg-burrage/template1.js
@@ -1157,7 +1157,7 @@ window.templateCustomizationSchema = {
                ],
                features: [
                   {
-                     name: "Replace Content With Video",
+                     name: "Video",
                      key: "template-hero-right-video",
                      isTurnedOn: false,
                      customizations: [

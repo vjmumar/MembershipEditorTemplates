a12577149129387d25e8375b20c40f8fc362c798
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-03-31 20:21:12 +0800

    c

--- src/clientTemplates/meg-burrage/template1.js
+++ src/clientTemplates/meg-burrage/template1.js
@@ -1271,7 +1271,7 @@ window.templateCustomizationSchema = {
                         let js = `
                         (() => {
                            window['template-hero-right-previous-html'] = document.querySelector('.template-hero__right').innerHTML;
-                           document.querySelector('.template-hero__right').innerHTML =  '<video src="{{template-hero-right-video-url}}" playsinline="" controls="" poster="{{template-hero-right-video-thumbnail}}"></video>'; 
+                           document.querySelector('.template-hero__right').innerHTML =  '<video style="width: 100%;" src="{{template-hero-right-video-url}}" playsinline="" controls="" poster="{{template-hero-right-video-thumbnail}}"></video>'; 
                         })();
                         `;
                         customizations.forEach((cItem) => {

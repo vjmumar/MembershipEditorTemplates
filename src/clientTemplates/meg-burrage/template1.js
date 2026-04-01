de2fff545a8586ae10945008ea2a3be2a8cc5829
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-04-01 13:03:07 +0800

    c

--- src/clientTemplates/meg-burrage/template1.js
+++ src/clientTemplates/meg-burrage/template1.js
@@ -4633,7 +4633,7 @@ class CourseTemplate {
                     
                         <!-- This is the container that will be toggled by JavaScript -->
                         <div class="template-community-embed" id="community-embed-area">
-                            ${embedHTML || `<iframe src="/communities/groups/test/channels/Announcements-4yNCt7"></iframe>`}
+                            ${embedHTML || `<iframe src="/communities/groups"></iframe>`}
                         </div>
                     </div>
                 `;

0b504a841eab061e9e7aae4631206d57f8cdaf60
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-04-28 11:18:32 +0800

    c

--- src/interceptorUnstable.js
+++ src/interceptorUnstable.js
@@ -9,75 +9,57 @@ export default {
             host: ghlOrigin,
          },
       });
-      const getCookie = (name = "") => {
-         const cookie = request.headers.get("Cookie");
-         return cookie
-            ?.split(";")
-            ?.find((e) => e.includes(`${name}=`))
-            ?.split("=")[1]
-            ?.trim();
-      };
-      const token = getCookie("cat");
-      const productId = getCookie("productId");
-      const locationId = getCookie("locationId");
       const contentType = ghlResponse.headers.get("content-type") || "";
       if (contentType.includes("text/html")) {
          let html = await ghlResponse.text();
-         let parsedToken = JSON.parse(atob(token));
-         let script = `
-            <script>window.cookieStore.set("acat",'${token}')</script>
+         const getCookie = (name = "") => {
+            const cookie = request.headers.get("Cookie");
+            return cookie
+               ?.split(";")
+               ?.find((e) => e.includes(`${name}=`))
+               ?.split("=")[1]
+               ?.trim();
+         };
+         const token = getCookie("cat");
+         const productId = getCookie("productId");
+         const locationId = getCookie("locationId");
+         let script = "";
+         if (token) {
+            const product = await (async () => {
+               const tokenJson = JSON.parse(atob(token));
+               const url = `https://services.leadconnectorhq.com/membership/locations/${locationId}/products/${productId}`;
+               const productRes = await fetch(url, {
+                  headers: {
+                     "accept": "application/json, text/plain, */*",
+                     "accept-language": "en-US,en;q=0.6",
+                     "authorization": `Bearer ${tokenJson?.tokenId}`,
+                     "channel": "APP",
+                  },
+                  body: null,
+                  method: "GET",
+               })
+                  .then((e) => e.json())
+                  .then((e) => {
+                     return e;
+                  });
+               return productRes;
+            })();
+            script = `
+            <script>window.cookieStore.set("acat",'${token}');</script>
+            <script>${product.customJs}</script>
+            ${product.customHeader}
             <script>
                const url = "${url.href}";
-               const fetchProduct = async () => {
-                     const productId = '${parsedToken.productId}';
-                     const locationId = '${parsedToken.locationId}';
-                     const token = auth?.tokenId;
-                     return await new Promise((resolved, reject) => {
-                        const url = 'https://services.leadconnectorhq.com/membership/locations/${locationId}/products/${productId}';
-                        if (token) {
-                           fetch(url, {
-                              headers: {
-                                 "accept": "application/json, text/plain, */*",
-                                 "accept-language": "en-US,en;q=0.6",
-                                 "authorization": 'Bearer ${parsedToken.tokenId}',
-                                 "channel": "APP",
-                                 "priority": 'high',
-                              },
-                              body: null,
-                              method: "GET",
-                           })
-                              .then((e) => e.json())
-                              .then((e) => {
-                                 resolved(e);
-                              });
-                        } else {
-                           console.log("No Token Found! intercept");
-                        }
-                     });
-                  };
-               if (url.includes('/products/')) {               
+               if (url.includes('/products/')) {         
                   setTimeout(()=>document.body.classList.add("template-ready"),60000);
-                  fetchProduct().then((data) => {
-                      const parser = new DOMParser();
-                      const doc = parser.parseFromString(data.customHeader, 'text/html');
-                      const parsed = doc.querySelector('script');
-                      const $headerScript = document.createElement('script');
-                      const $customJsScript = document.createElement('script');
-                      $headerScript.innerHTML = parsed.innerHTML;
-                      $customJsScript.innerHTML = data.customJs;
-                      document.body.append($customJsScript,$headerScript);
-                      console.log('-');
-                  });
                }
             </script>
          `;
+         }
          const css = url.href.includes("/products/")
             ? `<style>body:not(.template-ready){opacity:0!important}</style>`
             : "";
-         html = html.replace(
-            "<head>",
-            `<head>${JSON.stringify(parsedToken)}${css}${script}`,
-         );
+         html = html.replace("<head>", `<head>${css}${script}`);
          return new Response(html, {
             status: ghlResponse.status,
             headers: { "content-type": "text/html" },

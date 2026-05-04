a8c0ddaf1f6e4bdc4515a82bee7c0c1e9d242eed
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-05-04 13:52:12 +0800

    Update interceptor.js

--- src/interceptor.js
+++ src/interceptor.js
@@ -13,7 +13,8 @@ export default {
       if (contentType.includes("text/html")) {
          let html = await ghlResponse.text();
          let script = "";
-         if (url.href.includes("/courses/products")) {
+         let isProduct = url.href.includes("/courses/products");
+         if (isProduct) {
             const getCookie = (name = "") => {
                const cookie = request.headers.get("Cookie");
                return cookie
@@ -42,61 +43,61 @@ export default {
             const token = acat || cat;
             const parsedToken = decodeToken(token);
             script = `
-            <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.10/dist/dotlottie-wc.js" type="module"></script>
-            <script>
-               const url = '${url.href}';
-               const fetchProduct = async () => {
-                     return await new Promise((resolved, reject) => {
-                        const url = 'https://services.leadconnectorhq.com/membership/locations/${locationId}/products/${productId}?test=true';
-                        fetch(url, {
-                              headers: {
-                                 "accept": "application/json, text/plain, */*",
-                                 "accept-language": "en-US,en;q=0.6",
-                                 "authorization": 'Bearer ${parsedToken?.tokenId}',
-                                 "channel": "APP",
-                              },
-                              "priority": 'high',
-                              body: null,
-                              method: "GET",
-                           })
-                           ?.then((e) => e.json())
-                           ?.then((e) => {
-                              resolved(e);
-                           });
+               <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.10/dist/dotlottie-wc.js" type="module"></script>
+               <script>
+                  const url = '${url.href}';
+                  const fetchProduct = async () => {
+                        return await new Promise((resolved, reject) => {
+                           const url = 'https://services.leadconnectorhq.com/membership/locations/${locationId}/products/${productId}';
+                           fetch(url, {
+                                 headers: {
+                                    "accept": "application/json, text/plain, */*",
+                                    "accept-language": "en-US,en;q=0.6",
+                                    "authorization": 'Bearer ${parsedToken?.tokenId}',
+                                    "channel": "APP",
+                                 },
+                                 "priority": 'high',
+                                 body: null,
+                                 method: "GET",
+                              })
+                              ?.then((e) => e.json())
+                              ?.then((e) => {
+                                 resolved(e);
+                              });
+                        });
+                     };
+                  if (url.includes('/products/')) {               
+                     setTimeout(()=>document.body.classList.add("template-ready"),60000);
+                     fetchProduct().then((data) => {
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(data.customHeader, 'text/html');
+                        const parsed = doc.querySelector('script');
+                        const $headerScript = document.createElement('script');
+                        const $customJsScript = document.createElement('script');
+                        $headerScript.innerHTML = parsed?.innerHTML;
+                        $customJsScript.innerHTML = data?.customJs;
+                        document.body.append($customJsScript,$headerScript);
+                        console.log('-');
                      });
-                  };
-               if (url.includes('/products/')) {               
-                  setTimeout(()=>document.body.classList.add("template-ready"),60000);
-                  fetchProduct().then((data) => {
-                      const parser = new DOMParser();
-                      const doc = parser.parseFromString(data.customHeader, 'text/html');
-                      const parsed = doc.querySelector('script');
-                      const $headerScript = document.createElement('script');
-                      const $customJsScript = document.createElement('script');
-                      $headerScript.innerHTML = parsed.innerHTML;
-                      $customJsScript.innerHTML = data.customJs;
-                      document.body.append($customJsScript,$headerScript);
-                      console.log('-');
-                  });
-               }
-            </script>
-         `;
+                  }
+               </script>
+            `;
          }
          const css = "";
          const loader = `
-            <div
-              class="template-loader"
-              style="position:fixed;top:0;right:0;bottom:0;left:0;z-index:2147483647;display:grid;place-items:center;background:#fff;"
-            >
-              <dotlottie-wc
-                src="https://lottie.host/5947091c-caae-4663-94b5-a68817f9e24e/Tkb1qTZsPV.lottie"
-                style="width:150px;height:150px"
-                autoplay
-                loop
-              ></dotlottie-wc>
-            </div>
+               <div
+               class="template-loader"
+               style="position:fixed;top:0;right:0;bottom:0;left:0;z-index:2147483647;display:${isProduct ? "grid" : "block"};place-items:center;background:#fff;"
+               >
+               <dotlottie-wc
+                  src="https://lottie.host/5947091c-caae-4663-94b5-a68817f9e24e/Tkb1qTZsPV.lottie"
+                  style="width:150px;height:150px"
+                  autoplay
+                  loop
+               ></dotlottie-wc>
+               </div>
 
-         `;
+            `;
          html = html
             .replace("<head>", `<head>${css}${script}`)
             .replace("<body>", `<body>${loader}`);

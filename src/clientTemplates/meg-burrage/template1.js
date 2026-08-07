ef3c47c588ed096ddacc7c35016e65c335d57ac8
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-08-07 15:22:40 +0800

    c

--- src/clientTemplates/meg-burrage/template1.js
+++ src/clientTemplates/meg-burrage/template1.js
@@ -4200,7 +4200,7 @@ class CourseTemplate {
          if (Object.keys(previousData).length > 0 && previousData?.id === productId)
             return previousData;
          return await new Promise((resolved, reject) => {
-            const url = `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/products/${productId}`;
+            const url = `https://services.leadconnectorhq.com/memberships/locations/${locationId}/products/${productId}`;
             if (token) {
                fetch(url, {
                   headers: {
@@ -4241,7 +4241,7 @@ class CourseTemplate {
             const categoryId =
                catId ||
                location.href.split("/categories/")[1].split("?")[0].split("/")[0];
-            const url = `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/user-purchase/categories/${categoryId}?product_id=${productId}&visibility=published&published_posts=true`;
+            const url = `https://services.leadconnectorhq.com/memberships/locations/${locationId}/user-purchase/categories/${categoryId}?product_id=${productId}&visibility=published&published_posts=true`;
             if (token) {
                fetch(url, {
                   headers: {
@@ -4302,7 +4302,7 @@ class CourseTemplate {
          const previousData = JSON.parse(sessionStorage.getItem(storageName) || "[]");
          if (previousData.length > 0) return previousData;
          return await new Promise((resolved, reject) => {
-            const url = `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/user-purchase/categories?product_id=${productId}`;
+            const url = `https://services.leadconnectorhq.com/memberships/locations/${locationId}/user-purchase/categories?product_id=${productId}`;
             if (token) {
                fetch(url, {
                   headers: {
@@ -4341,7 +4341,7 @@ class CourseTemplate {
          return await new Promise((resolved, reject) => {
             const postId =
                pId || location.href.split("/posts/")[1].split("?")[0].split("/")[0];
-            const url = `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/posts/${postId}`;
+            const url = `https://services.leadconnectorhq.com/memberships/locations/${locationId}/posts/${postId}`;
             if (postId) {
                fetch(url, {
                   headers: {
@@ -4378,7 +4378,7 @@ class CourseTemplate {
          if (previousData.length > 0) return previousData;
          return await new Promise(async (resolved, reject) => {
             if (token) {
-               const url = `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/user-post-completion?product_id=${productId}&user_id=${userId}`;
+               const url = `https://services.leadconnectorhq.com/memberships/locations/${locationId}/user-post-completion?product_id=${productId}&user_id=${userId}`;
                fetch(url, {
                   headers: {
                      "accept": "application/json, text/plain, */*",
@@ -4419,7 +4419,7 @@ class CourseTemplate {
          return await new Promise(async (resolved, reject) => {
             if (token) {
                fetch(
-                  `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/products/user-activity/${cId || contactId}`,
+                  `https://services.leadconnectorhq.com/memberships/locations/${locationId}/products/user-activity/${cId || contactId}`,
                   {
                      headers: {
                         "accept": "application/json, text/plain, */*",
@@ -4461,7 +4461,7 @@ class CourseTemplate {
             if (token) {
                const categoryIds = await this.data.fetchCategories();
                fetch(
-                  `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/categories/get-progress`,
+                  `https://services.leadconnectorhq.com/memberships/locations/${locationId}/categories/get-progress`,
                   {
                      headers: {
                         "accept": "application/json, text/plain, */*",
@@ -4548,7 +4548,7 @@ class CourseTemplate {
          try {
             if (isComplete) {
                req = await fetch(
-                  `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/user-post-completion`,
+                  `https://services.leadconnectorhq.com/memberships/locations/${locationId}/user-post-completion`,
                   {
                      method: "POST",
                      headers: {
@@ -4571,7 +4571,7 @@ class CourseTemplate {
                );
             } else {
                req = await fetch(
-                  `https://services.leadconnectorhq.com/clientportal-middleware/memberships/locations/${locationId}/user-post-completion/${postId}`,
+                  `https://services.leadconnectorhq.com/memberships/locations/${locationId}/user-post-completion/${postId}`,
                   {
                      headers: {
                         "accept": "application/json, text/plain, */*",

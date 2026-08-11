5a7c7affa7fa34dd8ac77d5b2d85d0bbb9f98de6
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-08-11 14:00:35 +0800

    c

--- src/clientTemplates/meg-burrage/template-1/index.js
+++ src/clientTemplates/meg-burrage/template-1/index.js
@@ -2990,7 +2990,7 @@ class CourseTemplate {
          // Finally we will render the Navbar
          const html = `
                     <header class="template-navbar-wrapper" id="custom-navbar">
-                        <div onclick="window.CourseTemplate.coreMethods.actions.navigate('landingPage', {categoryId: '${c.id}'})" class="template-navbar__logo-link"><img src="${logo}" /></div>
+                        <div onclick="window.CourseTemplate.coreMethods.actions.navigate('landingPage')" class="template-navbar__logo-link"><img src="${logo}" /></div>
                         <!-- Center: Navigation Links -->
                         <div class="template-navbar__content">
                             <i class="fa-solid fa-xmark template-navbar__content__close" onclick="this.parentElement.classList.remove('active')"></i>

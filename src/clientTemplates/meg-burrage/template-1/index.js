e5fcf4b531b873b1cf72f4d3e7b38763ea977aba
Virgilio N. Mumar Jr <63900047+vjmumar@users.noreply.github.com>
2026-08-12 13:02:43 +0800

    c

--- src/clientTemplates/meg-burrage/template-1/index.js
+++ src/clientTemplates/meg-burrage/template-1/index.js
@@ -3357,6 +3357,7 @@ class CourseTemplate {
             this.coreMethods.data.fetchCategories(),
          ]).then((res) => res.map((e) => e.value));
 
+         console.log(currentPost);
          // Then we will create the bread crumbs
          const breadCrumbs = await (async () => {
             // const $el = await this.coreMethods.utils.waitForElement(
